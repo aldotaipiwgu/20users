c3e33e4b5c692c36013dd5e961c412bab707af2f
aldotaipiwgu <127356823+aldotaipiwgu@users.noreply.github.com>
2023-03-09 20:22:16 -0500

    Update 20user.ps1

--- 20user.ps1
+++ 20user.ps1
@@ -2,7 +2,7 @@
 $numberOfComputers = 10
 
 # Set the OU where the computer accounts will be created
-$ouPath = "OU=Computers,DC=home,DC=lab,DC=com"
+$ouPath = "OU=Computers1,DC=home,DC=lab,DC=com"
 
 # Loop through and create the computer accounts
 for ($i = 1; $i -le $numberOfComputers; $i++) {
